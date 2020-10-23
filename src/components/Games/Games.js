5a46c5ccf6ca05de9b9c24c084e61cf81d10ac6a
Romain <septier.romain@gmail.com>
2020-10-23 14:38:57 +0200

    format size of url image

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -4,10 +4,21 @@ import api from '../../api'
 function Games() {
 
   const [games, setGames] = useState([])
+
   useEffect(()=> {
     const fetchData = async () => {
       const result = await api.get('https://api.twitch.tv/helix/games/top')
-      console.log(result);
+      // console.log(result);
+
+      let dataArray = result.data.data
+      let finalArray = dataArray.map(game=>{
+        let newUrl = game.box_art_url
+        .replace('{width}', '250')
+        .replace('{height}', '300')
+      game.box_art_url = newUrl
+      return game
+      })
+      setGames(finalArray)
     }
     fetchData()
   }, [])
