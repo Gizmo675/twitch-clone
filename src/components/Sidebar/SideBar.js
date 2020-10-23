6d0b149cd2e6c950d4e316cdeda57314b163a633
Romain <septier.romain@gmail.com>
2020-10-23 13:33:30 +0200

    sidebar jsx & css

---
+++ src/components/Sidebar/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sidebar() {
+
+  return (
+    <div className="sidebar">
+      <h2 className="titreSidebar">Chaines recommandées</h2>
+      <ul className="listeStream"></ul>
+    </div>
+  )
+}
+export default Sidebar
